a6681b8d16b8f79788b36e566ca91afd7e02684b
tezarsurya <tzr.surya@gmail.com>
2023-02-22 14:44:34 +0700

    add mobile nav, recent works section and footer

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
-const form = document.getElementById("message-me");
 const header = document.querySelector("header");
+const form = document.getElementById("contact-form");
+const heroCta = document.getElementsByClassName("hero-cta");
+const navBtn = document.getElementById("nav-button");
+const mobileNav = document.getElementById("mobile-nav");
+const bars = navBtn.children;
+const overlay = document.getElementById("nav-overlay");
+
+let mobileNavShow = false;
+
+for (const cta of heroCta) {
+  cta.addEventListener("click", (e) => {
+    e.preventDefault();
+    const heroHeight = document.getElementById("hero").offsetHeight;
+
+    window.scrollTo({
+      top: heroHeight,
+      behavior: "smooth",
+    });
+  });
+}
+
+function openNav() {
+  mobileNavShow = true;
+  navBtn.style.border = "2px solid var(--lightMagenta)";
+  navBtn.style.boxShadow = "0 0 20px var(--primary)";
+  overlay.style.visibility = "visible";
+  overlay.style.animation = "reveal 0.3s ease-out";
+  mobileNav.style.top = "4.5rem";
+}
+
+function closeNav() {
+  mobileNavShow = false;
+  navBtn.removeAttribute("style");
+  overlay.style.animation = "conceal 0.3s ease-out, hide 1s infinite 0.3s";
+  setTimeout(() => {
+    overlay.removeAttribute("style");
+  }, 300);
+  mobileNav.removeAttribute("style");
+}
+
+navBtn.addEventListener("click", (e) => {
+  if (!mobileNavShow) {
+    return openNav();
+  }
+  return closeNav();
+});
+
+overlay.addEventListener("click", (e) => closeNav());
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -11,6 +58,14 @@ form.addEventListener("submit", (e) => {
   };
   e.currentTarget.reset();
   alert(JSON.stringify(data, 4, null));
+});
+
+window.addEventListener("resize", (e) => {
+  if (window.screen.width <= 640) {
+    header.style.top = 0;
+  } else {
+    header.style.top = "-7rem";
+  }
 });
 
 window.addEventListener("scroll", (e) => {
